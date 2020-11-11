ee4b9de9878e0fb40bff2bb8f3509b8f1387a693
k0814 <73882547+k0814@users.noreply.github.com>
2020-11-11 11:44:12 +0300

    Add files via upload

---
+++ oddoreven.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a;
+cout<<"Enter Number"<<endl;
+cin>>a;
+if(a% 2==0)
+	cout<<a<<" is an Even number"<<endl;
+else
+	cout<<a<<" is an odd number";
+return 0;
+	
+}
+
